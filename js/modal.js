191b2e071f4788e687179f0a523f8a46786d5a02
Elswortz <elswortz@gmail.com>
2025-01-30 01:12:59 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
 const refs = {
-  openModalBtn: document.querySelector(".hero-btn"),
-  closeModalBtn: document.querySelector(".modal-close"),
-  backdrop: document.querySelector(".backdrop"),
+  openModalBtn: document.querySelector("[data-modal-open]"),
+  closeModalBtn: document.querySelector("[data-modal-close]"),
+  backdrop: document.querySelector("[data-modal]"),
 };
 
 refs.openModalBtn.addEventListener("click", toggleModal);
