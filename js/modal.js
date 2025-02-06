b5cdcb658f35bc4a7597ed720d39d3c01b14a73e
Elswortz <elswortz@gmail.com>
2025-02-06 21:41:41 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -9,5 +9,5 @@ refs.closeModalBtn.addEventListener("click", toggleModal);
 refs.backdrop.addEventListener("click", logBackdropClick);
 
 function toggleModal() {
-  refs.backdrop.classList.toggle("is-hidden");
+  refs.backdrop.classList.toggle('backdrop--is-hidden');
 }
