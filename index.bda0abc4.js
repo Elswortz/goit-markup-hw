08a11050eacb2502a2ec87c79c3393529691ea82
Elswortz <62616411+Elswortz@users.noreply.github.com>
2025-02-11 19:58:59 +0000

    Deploying to gh-pages from @ Elswortz/goit-markup-hw@19fac9635be1b62ad29ed2bfeb90bd132214ff68 🚀

---
+++ index.bda0abc4.js
@@ -0,0 +1,2 @@
+var refs={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),backdrop:document.querySelector("[data-modal]")};function toggleModal(){refs.backdrop.classList.toggle("backdrop--is-hidden")}refs.openModalBtn.addEventListener("click",toggleModal),refs.closeModalBtn.addEventListener("click",toggleModal),refs.backdrop.addEventListener("click",logBackdropClick);
+//# sourceMappingURL=index.bda0abc4.js.map
